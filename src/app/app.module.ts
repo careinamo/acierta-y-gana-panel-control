83a3c194d0b6268ac52cc73dd921717425f4753f
christian areinamo <careinamoandrade@gmail.com>
2019-02-02 15:25:05 -0400

    primer commit. Creacion de componentes basico, instalacion de angular material y creacion de rutas basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Routes
+import { APP_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    PagesComponent,
+    RegisterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
