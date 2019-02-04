c3166a627d83d7426da68170ced5c60288824ae6
christian areinamo <careinamoandrade@gmail.com>
2019-02-04 00:03:55 -0400

    sin comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Routes
 import { APP_ROUTES } from './app.routes';
 
 // Modules
-import { PagesModule } from './pages/pages.modules';
-
-
+import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceModule } from './services/service.module';
 
 @NgModule({
   declarations: [
@@ -23,9 +20,10 @@ import { RegisterComponent } from './login/register.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     PagesModule,
     APP_ROUTES,
-    BrowserAnimationsModule
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,6 +2,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { ConcursosComponent } from './concursos/concursos.component';
+import { ConcursoComponent } from './concurso/concurso.component';
 
 const pagesRoutes: Routes = [
     {
@@ -9,6 +11,8 @@ const pagesRoutes: Routes = [
         component: PagesComponent,
         children: [
             { path: 'dashboard', component: DashboardComponent },
+            { path: 'concursos', component: ConcursosComponent },
+            { path: 'concurso/:id', component: ConcursoComponent },
             { path: 'graficas1', component: Graficas1Component },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
         ]
